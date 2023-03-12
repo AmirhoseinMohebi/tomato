6a5520c065360a0e6dc2a0655283ddeff141aa89
AmirhoseinMohebi <127586801+AmirhoseinMohebi@users.noreply.github.com>
2023-03-13 01:44:47 +0330

    update

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-
 window = Tk()
 window.title('Tomato')
 window.config(padx=100, pady=50, bg=PINK)
+
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=PINK)
+title_label.grid(column=1, row=0)
 
 
 canvas = Canvas(width=200, height=224, bg=PINK, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.pack()
 canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 35, "bold"))
-
+canvas.grid(column=1, row=1)
 
 window.mainloop()
